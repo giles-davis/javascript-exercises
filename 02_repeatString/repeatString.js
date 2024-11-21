c62892ded784d1662dd412dd6d932f61aecbe9b7
Mitch Marshall <mtch.marshall@gmail.com>
2024-11-21 12:43:27 +1100

    Completed exercises 1,2,3.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    console.log("FUNCTION CALLED!");
+    console.log(`Input string: ${string}, num: ${num}`);
+    
+    if (num === 0) {
+        return "";
+    } else if (num < 0) {
+        return "ERROR"
+    }
+    let result = '';
+    
+    for (let i = 0; i < num; i++){
+        result += string;
+        console.log(`Loop ${i}: ${result}`);
+    }
+    
+    console.log(`Final result: ${result}`);
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
