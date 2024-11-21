109569a4861becad0e640e9d0574ad024dae9f0c
Mitch Marshall <mtch.marshall@gmail.com>
2024-11-22 09:22:04 +1100

    Completed exercise 7!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(numF) {
+  let conversion = (numF - 32) * 5/9;
+  return Number(conversion.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(numC) {
+  let conversion = numC * (9/5) + 32;
+  return Number(conversion.toFixed(1))
 };
 
-// Do not edit below this line
+
+
+console.log(convertToCelsius(86).toFixed(1));
+console.log(convertToFahrenheit(23).toFixed(1));
+
+// // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
