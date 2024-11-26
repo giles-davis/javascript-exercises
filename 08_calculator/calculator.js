4481078c0a88053eb578bf849d41e9d21c78e85a
Mitch Marshall <mtch.marshall@gmail.com>
2024-11-26 15:46:29 +1100

    Completed exercises!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = (numbers) => {
+  return numbers.reduce((total, num) => total + num, 0);
+}
+
+const multiply = (numbers) => {
+  return numbers.reduce((total, num) => total * num, 1);
 };
 
-const multiply = function() {
-
+const power = (base, exponent) => {
+  return Math.pow(base, exponent);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function fact(num) {
+  let res = 1; 
+    
+  if(num === 0)
+      return 1;
+  for (let i = 2; i <= num; i++) 
+      res = res * i; 
+  return res; 
 	
 };
 
---
+++ objects.js
@@ -0,0 +1,17 @@
+let technoproducers = {
+    producer1: {
+        name: "Chlar",
+        genre: "Hypnotic Techno",
+    },
+    producer2: {    
+        name: "Shlomo",
+        genre: "Hard Techno",
+    }
+};
+
+for (let key in technoproducers) {
+    alert(producer1.name);
+    alert(producer1.genre);
+    alert(producer2.name);
+    alert(producer2.genre);
+}
