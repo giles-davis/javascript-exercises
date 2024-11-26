0fb721e73e892a7262441331899c8b5ca88ee091
Mitch Marshall <mtch.marshall@gmail.com>
2024-11-26 16:48:08 +1100

    Completed task!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphnumeric = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
+
+const cleanedString = string
+.toLowerCase()
+.split('')
+.filter((character) => alphnumeric.includes(character))
+.join('');
+
+const reversedString = cleanedString.split('').reverse().join('');
+
+return cleanedString === reversedString;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+  // Since we only consider letters and numbers, create a variable containing all valid characters
+  // Convert to lowercase, split to array of individual characters, filter only valid characters, then rejoin as new string
+  // Create a new reversed string for comparison
+  // Return the outcome of the comparison which will either be true or false
